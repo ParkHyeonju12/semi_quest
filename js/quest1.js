b72d9fbf3c84fb676a2b5d440efde488c7e85bfb
DESKTOP-3DIAQTA\user1 <ka7417@naver.com>
2022-09-16 18:17:27 +0900

    [update] quest1.html

---
+++ js/quest1.js
@@ -0,0 +1,24 @@
+const poly = $(".poly");
+const city = $(".city");
+const white = $(".white");
+const wt = $(".with");
+const cityLi = $(".city_wrap>li");
+let interval;
+
+//퀘스트 수행 장소 글씨 마우스 호버시 효과 적용
+cityLi.mouseenter(function(){
+    const index = $(this).index();
+    poly.eq(index).css("display","block");
+    white.eq(index).css("color","white");
+    wt.eq(index).css("display","block");
+    interval = setInterval(function(){
+        poly.eq(index).toggle();
+    },400);
+});
+cityLi.mouseleave(function(){
+    const index = $(this).index();
+    poly.eq(index).css("display","none");
+    white.eq(index).css("color","transparent");
+    wt.eq(index).css("display","none");
+    clearInterval(interval);
+})
